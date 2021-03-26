a3c473b2026c0a8c269ffb3f152aa2e5d0094585
guy vitrak <guy.vitrak@live.biu.ac.il>
2021-03-26 13:30:50 +0300

    Updated IDataBase

--- AP2_Ex1/IDatabase.cs
+++ AP2_Ex1/IDatabase.cs
@@ -6,10 +6,30 @@ namespace AP2_Ex1
 {
     public interface IDatabase
     {
+        /// <summary>
+        /// Returns a dictionary with contained info of given line
+        /// </summary>
+        /// <param name="lineNumber">Line number we want to get</param>
+        /// <returns>info</returns>
         Dictionary<String, double> getLine(int lineNumber);
+
+        /// <summary>
+        /// Get the line as a string, as it was in the CSV file.
+        /// </summary>
+        /// <param name="lineNumber">Line number we want to get</param>
+        /// <returns>The string</returns>
         string getLineString(int lineNumber);
 
-        List<string> Properties { get; }
-        List<double> getPropertyArray(string name);
+        /// <summary>
+        /// The keys we have, list of strings.
+        /// </summary>
+        IList Keys { get; }
+
+       /// <summary>
+       /// Get a list of all the data under the given property, sorted as it was in  the CSV file.
+       /// </summary>
+       /// <param name="key">key we want to get</param>
+       /// <returns>List of doubles od the data</returns>
+        IList getPropertyArray(string key);
     }
 }
