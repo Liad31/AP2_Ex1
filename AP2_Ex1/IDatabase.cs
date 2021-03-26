7f34ec1467c9a16cbf13e78b103c5565fea9ff88
Roy Cirkin <RoyCirkin1@gmail.com>
2021-03-26 11:45:06 +0300

    finished IDatabase

--- AP2_Ex1/IDatabase.cs
+++ AP2_Ex1/IDatabase.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace AP2_Ex1
 {
     public interface IDatabase
     {
         Dictionary<String, double> getLine(int lineNumber);
+        string getLineString(int lineNumber);
 
-        double [] getPropertyArray
+        List<string> Properties { get; }
+        List<double> getPropertyArray(string name);
     }
 }
