c96b3f6f10ec5714338db405bc2b82ae34fc100c
Liad Kehila <Liad3131@gmail.com>
2021-04-01 05:07:20 +0300

    added settings and now auto launches flight gear given the executable file

---
+++ AP2_Ex1/ApplicationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AP2_Ex1
+{
+    public class ApplicationSettings
+    {
+        public string FlightGearPath { get; set; }
+        public string FlightCsvPath { get; set; }
+        public string TrainingCsvPath { get; set; }
+        public ApplicationSettings(){}
+        public ApplicationSettings(string flightGearPath = null, string flightCsvPath = null, string trainingCsvPath = null)
+        {
+            this.FlightGearPath = flightGearPath;
+            this.FlightCsvPath = flightCsvPath;
+            this.TrainingCsvPath = trainingCsvPath;
+        }
+    }
+}
--- AP2_Ex1/StartWindow.xaml.cs
+++ AP2_Ex1/StartWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Xml;
+using System.Xml.Serialization;
 namespace AP2_Ex1
 {
     /// <summary>
@@ -22,45 +24,89 @@ namespace AP2_Ex1
     /// </summary>
     public partial class StartWindow : Window
     {
-        private string CSVFile = "";
-        private string XMLFile = "";
+        public ApplicationSettings  settings;
         public StartWindow()
         {
+            if (File.Exists("settings.xml"))
+            {
+                var serializer = new XmlSerializer(typeof(ApplicationSettings));
+                using (var reader = XmlReader.Create("settings.xml"))
+                {
+                    settings= (ApplicationSettings)serializer.Deserialize(reader);
+                }
+            }
+            else
+            {
+                settings = new ApplicationSettings();
+            }
             InitializeComponent();
+            this.DataContext = settings;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void FlightPathClick(object sender, RoutedEventArgs e)
         {
             var dlg = new OpenFileDialog();
+            dlg.Filter = "CSV files(*.csv *.xlsx )|*.csv;*.xlsx| Text files (*.txt)| *.txt";
             Nullable<bool> result = dlg.ShowDialog();
-
             if (result == true)
             {
-                // Save document
-                string filename = dlg.FileName;
-                this.CSVFile = filename;
-                this.dataFileBox.Text = filename;
+                FlightFileBox.Text= dlg.FileName;
             }
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void TrainingPathClick(object sender, RoutedEventArgs e)
         {
             var dlg = new OpenFileDialog();
+            dlg.Filter = "CSV files(*.csv *.xlsx )|*.csv;*.xlsx| Text files (*.txt)| *.txt";
+            Nullable<bool> result = dlg.ShowDialog();
+            
+            if (result == true)
+            {
+                TrainingFileBox.Text= dlg.FileName;
+            }
+        }
+        
+        private void FlightGearPathClick(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter="flight gear executable | fgfs.exe| executable files (*.exe)|*.exe";
             Nullable<bool> result = dlg.ShowDialog();
 
             if (result == true)
             {
-                // Save document
-                string filename = dlg.FileName;
-                this.XMLFile = filename;
-                this.XMLFileBox.Text = filename;
+                FlightGearPathBox.Text= dlg.FileName;
             }
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void StartSimulation(object sender, RoutedEventArgs e)
         {
-            var mw = new MainWindow(CSVFile, XMLFile);
+            var serializer = new XmlSerializer(settings.GetType());
+            using (var writer = XmlWriter.Create("settings.xml"))
+            {
+                serializer.Serialize(writer, settings);
+            }
+            //if the user never intended to open flight gear there's no reason to try or display a message box
+            if (settings.FlightGearPath !=null) 
+            {
+                try
+                {
+                    var startInfo = new ProcessStartInfo();
+                    string fgPath = settings.FlightGearPath;
+                    startInfo.FileName = fgPath;
+                    int indexOfLastSlash = fgPath.LastIndexOf("\\", StringComparison.Ordinal);
+                    string workingDirectory = fgPath.Substring(0, indexOfLastSlash);
+                    startInfo.WorkingDirectory = workingDirectory;
+                    //TODO: insert actual launch arguments 
+                    startInfo.Arguments = "";
+                    Process proc = Process.Start(startInfo);
+                }
+                catch
+                {
+                   MessageBox.Show("flight gear path is invalid");
+                }
+            }
+            var mw = new MainWindow(settings.FlightCsvPath, settings.TrainingCsvPath);
             mw.Show();
             this.Close();
         }
