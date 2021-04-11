1818ebaae653fc7ebff34c40f429bd41cde26201
Liad Kehila <Liad3131@gmail.com>
2021-04-11 18:36:11 +0300

    deleted useless files

--- CsTest/Program.cs
+++ CsTest/Program.cs
@@ -7,12 +7,12 @@ namespace Sandbox
     class Program
     {
         //Entity(String^ name, float xPos, float yPos);
-        
+
         static void Main(string[] args)
         {
             var dll = Assembly.LoadFile(@"C:\Users\liad3\source\repos\DLLmaybe\Debug\Wrapper.dll");
             object[] argsEntity = { @"E:\Users\Liad\Downloads\reg_flight.csv" };
-            dynamic ts = Activator.CreateInstance(dll.GetType("Wrapper.TimeSeries"),argsEntity);
+            dynamic ts = Activator.CreateInstance(dll.GetType("Wrapper.TimeSeries"), argsEntity);
             dynamic SimpleAd = Activator.CreateInstance(dll.GetType("Wrapper.SimpleAnomalyDetector"));
             SimpleAd.learnNormal(ts);
             var arr = SimpleAd.detect(@"E:\Users\Liad\Downloads\anomaly_flight.csv");
